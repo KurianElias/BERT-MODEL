8b9cb7ad05508971e9306028a3654af906131a4f
KURIAN ELIAS <163384553+KurianElias@users.noreply.github.com>
2024-06-25 15:16:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,12 +100,12 @@ if st.button("Check Match Score"):
         jd_skills = extract_skills_jd(jd)
         st.success('Extracting skills from Job Description successful!')
         # Display extracted skills
-        if resume_skills:
-            st.write("**Resume Skills:**")
-            st.write(", ".join(resume_skills))
-        if jd_skills:
-            st.write("**Job Description Skills:**")
-            st.write(", ".join(jd_skills))
+        # if resume_skills:
+        #     st.write("**Resume Skills:**")
+        #     st.write(", ".join(resume_skills))
+        # if jd_skills:
+        #     st.write("**Job Description Skills:**")
+        #     st.write(", ".join(jd_skills))
         
         # Check match using BERT
         if resume_skills and jd_skills:
